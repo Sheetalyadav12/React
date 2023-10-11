ae611c203f2c8bd714297f7b2e13a4073167b833
Sheetal Yadav <sheetalyadav9988@gmail.com>
2023-10-11 14:23:54 +0530

    state with class components

--- src/App.js
+++ src/App.js
@@ -2,55 +2,50 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-// function myComponent(){
-
-//   return<h2>hello </h2>
-// }
-//class components
-// class App extends React.Component{
-//   render()
-
-//   {
-//     return<h1>this is a heading page</h1>
-//   }
-// }
-
- function App(props){
-  console.log(props.color);
-
-   
-    return (
-      <div className="App">
-        <button onClick={props.clickEvent}>click me!</button>
-
-      </div>
-      
-    );
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props)
+      console.log("Super Function");
+    
   }
-  // return myComponent();
 
-// const myComponent= () =>{
-//   return<h1>hello</h1>
-// }
-//    return myComponent();
- 
-  
-  
-  
-  
-  // return  React.createElement (
-  //   // "div",
-  //   // null,
-  //   // React.createElement("h1",null,"hello "),
-  //   // React.createElement("p",null,"this is a heading"),
-  //   // React.createElement("ul",null,""),
-  //   // React.createElement("li",null,"list item1"),
-  //   // React.createElement("li",null,"list item 2"),
+  state = { counter: 0 }
+  increase = () => {
+    this.setState({ counter: parseInt(this.state.counter) + 1 })
+  }
+
+  render() {
+    return(
+      <div>
+    <button onClick={this.increase}>Click Me</button>
+    Counter : {this.state.counter}
+    </div>
+    )
+
+  }
 
 
-    
-    
-  // );
+}
+
+
+// function App()
+
+
+
+
+
+// return (
+
+//   <div className='App'>
+//      <button onClick={props.clickEvent} >clicl me</button>
+//   </div>
+// );
+
+
+
+
+
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App  color="red" clickEvent={()=>{
-      console.log("click button!");
-    }}/>
+    <App color="blue" size="33" clickEvent={()=>{
+      console.log("who can click my button");
+      alert("who click button");
+
+    }
+    
+    }/>
   </React.StrictMode>
 );
 
